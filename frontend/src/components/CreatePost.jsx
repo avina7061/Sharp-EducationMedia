8e61fdb0115832d84a51faafbbe7ad6fa4485fb6
avina7061 <151718687+avina7061@users.noreply.github.com>
2025-02-08 22:47:45 +0530

    Update CreatePost.jsx

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -37,7 +37,7 @@ const CreatePost = ({ open, setOpen }) => {
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:8003/api/v1/post/addpost",
+        "https://sharp-educationmedia.onrender.com/api/v1/post/addpost",
         formData,
         {
           headers: {
